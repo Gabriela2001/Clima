4d3b91c44a47b0d7622bc6dc13d6ad2c5f8b664c
Gabriela2001 <rokagabriela@gmail.com>
2019-07-04 08:33:11 -0600

    arreglo repetición e input

--- Wheater/js/script.js
+++ Wheater/js/script.js
@@ -10,10 +10,13 @@ p.controller("ClimaCtrl",function($scope,$rootScope,$http){
 			url: "http://api.openweathermap.org/data/2.5/forecast?q="+y+",GT&APPID=f74006d77c6256a75842bc2ede1dda31"
 		}).then(function Datos(variable) {
 			console.log(variable)
-
+			$scope.Listado = [];
+			$scope.PaisIngresado="";
 			$scope.Listado.push(variable);
 		})
 	}
+
+
 })
 
 
